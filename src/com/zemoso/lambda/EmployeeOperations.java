f75cc449a233e7e6d0c2fad0c89b022576c0115d
SandyZemoso7 <sandeep.nishad@zemosolabs.com>
2023-05-24 15:15:06 +0530

    Java lambda expression amends done as per comment.

--- src/com/zemoso/lambda/EmployeeOperations.java
+++ src/com/zemoso/lambda/EmployeeOperations.java
@@ -4,10 +4,7 @@ import com.zemoso.lambda.pojo.Employee;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class EmployeeOperations {
 
@@ -16,33 +13,25 @@ public class EmployeeOperations {
         List<Employee> empList = new ArrayList<>();
 
         final String SOFTWARE_ENGINEER = "Software engineer";
+        final String SOFTWARE_TESTER = "Software Tester";
         EmployeeOperations employeeOperations = new EmployeeOperations();
 
         empList.add(new Employee("Sandeep", SOFTWARE_ENGINEER, 30000));
-        empList.add(new Employee("abhishek", SOFTWARE_ENGINEER, 20000));
+        empList.add(new Employee("abhishek", SOFTWARE_TESTER, 20000));
         empList.add(new Employee("sijin", SOFTWARE_ENGINEER, 20000));
-        empList.add(new Employee("bhavesh", SOFTWARE_ENGINEER, 50000));
+        empList.add(new Employee("bhavesh", SOFTWARE_TESTER, 50000));
 
-        // sorting based on employee name and storing inside map as particular object based on name as key
-        Map<String, Employee> employeeMap =	empList.stream()
-                .sorted((e1, e2) -> e1.getName().toLowerCase().compareTo(e2.getName().toLowerCase()))
-                .collect(Collectors.toMap(Employee::getName, Function.identity()));
-
-        System.out.println("-- Employees sorted by name");
-        employeeMap.forEach((k, v) -> System.out.println(k+" : "+v));
-
-        // sort by emp first name and filter by salary > 20000
-        employeeMap = empList.stream()
-                .sorted((e1, e2) -> e1.getName().toLowerCase().compareTo(e2.getName().toLowerCase()))
-                .filter(e -> e.getSalary() > 30000)
-                .collect(Collectors.toMap(Employee::getName, Function.identity()));
-
-        System.out.println("-- Employees with salary greater than 20000 and sorted by name:");
-        employeeMap.forEach((k, v) -> System.out.println(k+" : "+v));
+        List<Employee> filterByDesignation = employeeOperations.filterByJobDesignation(SOFTWARE_ENGINEER, empList);
+        System.out.println("-- Employees filtered by designation");
+        filterByDesignation.forEach(System.out::println);
 
         List<Employee> filteredBySalary = employeeOperations.filterBySalaryGreaterThan(20000, empList);
-        System.out.println("-- Employees with salary greater than 20000:");
+        System.out.println("-- Employees filtered by salary");
         filteredBySalary.forEach(System.out::println);
+
+        List<Employee> sortedByName = employeeOperations.sortByName(empList);
+        System.out.println("-- Employees sorted by name");
+        sortedByName.forEach(System.out::println);
     }
 
     public List<Employee> filterBySalaryGreaterThan(int salary, List<Employee> empList) {
@@ -50,4 +39,14 @@ public class EmployeeOperations {
         return empList.stream().filter(bySalary).toList();
     }
 
+    public List<Employee> filterByJobDesignation(String jobTitle, List<Employee> empList) {
+        Predicate<Employee> byJobTitle = e -> e.getDesignation().equalsIgnoreCase(jobTitle);
+        return empList.stream().filter(byJobTitle).toList();
+    }
+
+    public List<Employee> sortByName(List<Employee> empList) {
+        return empList.stream()
+                .sorted((e1, e2) -> e1.getName().toLowerCase().compareTo(e2.getName().toLowerCase()))
+                .toList();
+    }
 }
